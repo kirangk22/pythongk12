d93e2ce244e634035a1d80ed8db45b563f4d4dcd
kirangk22 <kiran.gk22@gmail.com>
2025-01-10 09:57:40 +0530

    variables.py

--- .venv/Scripts/com/pycode/demo/First1.py
+++ .venv/Scripts/com/pycode/demo/First1.py
@@ -1,2 +1,4 @@
+import pandas as pd
 name="kiran"
 print("Hiiii "+name)
+print(dir(pd))
---
+++ .venv/Scripts/com/pycode/demo/variables.py
@@ -0,0 +1,16 @@
+fname="kiran"
+lname="kumar"
+print(fname+" "+lname)
+
+
+age=35
+print("Age is:"+str(age))
+
+height=156.55
+print("height is:" + str(height) +"cm")
+
+is_human=True
+print("is humam:"+str(is_human))
+
+
+print(str(type(fname))+"-"+str(type(age))+"-"+str(type(height))+"-"+str(type(is_human)))
