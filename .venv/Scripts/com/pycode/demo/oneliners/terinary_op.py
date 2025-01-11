cde299e3207b671f14909eec02d2e9a11db18f68
kirangk22 <kiran.gk22@gmail.com>
2025-01-11 11:28:45 +0530

    python smaple

---
+++ .venv/Scripts/com/pycode/demo/oneliners/terinary_op.py
@@ -0,0 +1,12 @@
+#x if condition else y
+#return x if condition is true else return y
+
+n=5
+print("+ve" if n>0 else "-ve")
+
+age=30
+print("Adult" if age>18 else "child")
+
+
+n1,n2=4,8
+print("n1 is big" if n1>n2 else "n1 is small")
